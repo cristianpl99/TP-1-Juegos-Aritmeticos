d1b0a1a0e28a38b97dae3ae137778be10d5d3113
Cristian Pereyra <cristianpl99@gmail.com>
2023-03-22 22:11:47 -0300

    look and feel

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import tp.igu.Inicio;
 
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
 		Inicio inicio = new Inicio();
 		inicio.setVisible(true);
 		inicio.setLocationRelativeTo(null);
